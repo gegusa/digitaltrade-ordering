d207f24ab073de9b58ef50d7c982404a7fae25db
gegusa <gegusaa@yandex.ru>
2025-05-04 17:09:01 +0300

    добавил шину кафки, поправил конфиг

---
+++ DigitalTrade.Ordering.AppServices/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using DigitalTrade.Ordering.AppServices.Ordering;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DigitalTrade.Ordering.AppServices;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAppServices(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IOrderingHandler, OrderingHandler>();
+
+        return services;
+    }
+}
--- DigitalTrade.Ordering.Host/Program.cs
+++ DigitalTrade.Ordering.Host/Program.cs
@@ -1,17 +1,32 @@
+using DigitalTrade.Ordering.AppServices;
 using DigitalTrade.Ordering.Entities;
 using DigitalTrade.Ordering.Host.Extensions;
 using KafkaFlow;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
+builder.Services.AddMvc();
+builder.Services.AddControllers();
+
 builder.Services.AddEntities(configuration);
+builder.Services.AddAppServices(configuration);
 builder.Services.AddKafkaFlow(configuration);
 
+builder.Services.AddCors();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHealthChecks();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+app.MapControllers();
 var kafkaBus = app.Services.CreateKafkaBus();
 await kafkaBus.StartAsync();
 
